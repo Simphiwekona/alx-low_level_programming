cd79936b9caea9f0d0625be4e33099f73ddfc48f
Simphiwe Olwethu Kona <106931854+Simphiwekona@users.noreply.github.com>
2023-03-22 00:34:06 +0200

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_times_table - prints the n times table, starting with 0
- * @n: number of the times table
- */
+* print_times_table - prints the n times table, starting with 0
+* @n: number of the times table
+*/
 void print_times_table(int n)
 {
 int i, j, k;
@@ -18,7 +18,8 @@ k = j * i;
 if (j == 0)
 {
 _putchar(k + '0');
-} else if (k < 10 && j != 0)
+} 
+else if (k < 10 && j != 0)
 {
 _putchar(',');
 _putchar(' ');
@@ -33,7 +34,8 @@ _putchar(' ');
 _putchar(' ');
 _putchar((k / 10) + '0');
 _putchar((k % 10) + '0');
-} else if (k >= 100)
+} 
+else if (k >= 100)
 {
 _putchar(',');
 _putchar(' ');
