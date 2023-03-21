d4e045582d7a53ad8e7123ffb37cb1260a02d795
Simphiwe Olwethu Kona <106931854+Simphiwekona@users.noreply.github.com>
2023-03-22 01:05:18 +0200

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,49 +1,66 @@
 #include "main.h"
+
 /**
  * print_times_table - prints the n times table, starting with 0
  * @n: number of the times table
  */
 void print_times_table(int n)
 {
-int i, j, k;
-
 if (n >= 0 && n <= 15)
 {
-for (i = 0; i <= n; i++)
+for (int i = 0; i <= n; i++)
 {
-for (j = 0; j <= n; j++)
-{
-k = j * i;
-if (j == 0)
-{
-_putchar(k + '0');
+print_times_table_row(i, n);
 }
-else if (k < 10 && j != 0)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar(' ');
-_putchar(k + '0');
 }
-else if (k >= 10 && k < 100)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar((k / 10) + '0');
-_putchar((k % 10) + '0');
 }
-else if (k >= 100)
+
+/**
+ * print_times_table_row - prints a single row of the times table
+ * @i: the number to generate a times table row for
+ * @n: the highest multiplier to use in the row
+ */
+void print_times_table_row(int i, int n)
 {
-_putchar(',');
-_putchar(' ');
-_putchar((k / 100) + '0');
-_putchar(((k / 10) % 10) + '0');
-_putchar((k % 10) + '0');
-}
+for (int j = 0; j <= n; j++)
+{
+print_times_table_cell(i * j, j == 0);
 }
 _putchar('\n');
 }
+
+/**
+ * print_times_table_cell - prints a single cell of the times table
+ * @value: the value of the cell
+ * @first_column: whether or not this is the first column of the row
+ */
+void print_times_table_cell(int value, bool first_column)
+{
+if (!first_column)
+{
+_putchar(',');
+_putchar(' ');
+}
+
+if (value < 10)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');
+_putchar(value + '0');
+} 
+else if (value < 100)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar((value / 10) + '0');
+_putchar((value % 10) + '0');
+} 
+else 
+{
+_putchar(' ');
+_putchar((value / 100) + '0');
+_putchar(((value / 10) % 10) + '0');
+_putchar((value % 10) + '0');
 }
 }
