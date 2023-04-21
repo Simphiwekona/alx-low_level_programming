edd8a3041ced2924c06ffb402f0d7d8ebe85e3c1
Simphiwe Olwethu Kona <106931854+Simphiwekona@users.noreply.github.com>
2023-04-21 04:50:53 +0200

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,4 +1,4 @@
-include "variadic_functions.h"
+#include "variadic_functions.h"
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -10,36 +10,36 @@ include "variadic_functions.h"
   */
 void print_all(const char * const format, ...)
 {
-	va_list args;
-	f_dt form_types[] = {
-		{ "c", print_a_char },
-		{ "i", print_a_integer },
-		{ "f", print_a_float },
-		{ "s", print_a_char_ptr }
-	};
-	unsigned int i = 0;
-	unsigned int j = 0;
-	char *separator = "";
+va_list args;
+f_dt form_types[] = {
+{ "c", print_a_char },
+{ "i", print_a_integer },
+{ "f", print_a_float },
+{ "s", print_a_char_ptr }
+};
+unsigned int i = 0;
+unsigned int j = 0;
+char *separator = "";
 
-	va_start(args, format);
+va_start(args, format);
 
-	while (format != NULL && format[i])
-	{
-		j = 0;
-		while (j < 4)
-		{
-			if (format[i] == *form_types[j].identifier)
-			{
-				form_types[j].f(separator, args);
-				separator = ", ";
-			}
-			j++;
-		}
-		i++;
-	}
+while (format != NULL && format[i])
+{
+j = 0;
+while (j < 4)
+{
+if (format[i] == *form_types[j].identifier)
+{
+form_types[j].f(separator, args);
+separator = ", ";
+}
+j++;
+}
+i++;
+}
 
-	va_end(args);
-	printf("\n");
+va_end(args);
+printf("\n");
 }
 
 /**
@@ -51,7 +51,7 @@ void print_all(const char * const format, ...)
   */
 void print_a_char(char *separator, va_list args)
 {
-	printf("%s%c", separator, va_arg(args, int));
+printf("%s%c", separator, va_arg(args, int));
 }
 
 /**
@@ -63,7 +63,7 @@ void print_a_char(char *separator, va_list args)
   */
 void print_a_integer(char *separator, va_list args)
 {
-	printf("%s%i", separator, va_arg(args, int));
+printf("%s%i", separator, va_arg(args, int));
 }
 
 /**
@@ -75,7 +75,7 @@ void print_a_integer(char *separator, va_list args)
   */
 void print_a_float(char *separator, va_list args)
 {
-	printf("%s%f", separator, va_arg(args, double));
+printf("%s%f", separator, va_arg(args, double));
 }
 
 /**
@@ -87,13 +87,13 @@ void print_a_float(char *separator, va_list args)
   */
 void print_a_char_ptr(char *separator, va_list args)
 {
-	char *arg = va_arg(args, char *);
+char *arg = va_arg(args, char *);
 
-	if (arg == NULL)
-	{
-		printf("%s%s", separator, "(nil)");
-		return;
-	}
+if (arg == NULL)
+{
+printf("%s%s", separator, "(nil)");
+return;
+}
 
-	printf("%s%s", separator, arg);
+printf("%s%s", separator, arg);
 }
